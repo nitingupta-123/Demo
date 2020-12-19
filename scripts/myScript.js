c53e09044d5ec8593728db9c8ec2b58de1c133c5
nitingupta-123 <nitingupta03012000@gmail.com>
2020-12-19 17:53:43 +0530

    updating changes

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -17,37 +17,38 @@ function testGS() {
 function postFormData() {
 
     var url = "https://script.google.com/macros/s/AKfycbymFvPvFXLUJFlJ65d2emZhCIchgLhZzc-Izzi7l5kEgUQEi_g/exec";
-    const data=formData();
+    const data = formData();
     console.log(data);
-    fetch(url,{
+    fetch(url, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'no-cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        // credentials: 'omit', // include, *same-origin, omit
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cache
         headers: {
-          'Content-Type': 'application/json'
-          // 'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
         },
         redirect: 'follow', // manual, *follow, error
         // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         //data ={email:"nitingupta@gmail.com",name:"Nitin Gupta",password:"nitin@123"}
         body: JSON.stringify(data) // body data type must match "Content-Type" header
-      });
+    })
+        .then(response => response.json())
+        .then(data => console.log(data));
 
 }
 
-function formData(){
-    const name=document.getElementById('name').value;
-    const email=document.getElementById('email').value;
-    const password=document.getElementById('password').value;
-    const data={
-        name:name,
-        email:email,
-        password:password,
-        purpose:"signup"
+function formData() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const data = {
+        name: name,
+        email: email,
+        password: password,
+        purpose: "signup"
     }
     return data;
 }
 
 // document.getElementById("random").addEventListener("click",testGS);
-document.getElementById("signup").addEventListener("click",postFormData);
+document.getElementById("signup").addEventListener("click", postFormData);
